6e5382ddff64771bfbe9a53beb2cee14bc9b4c47
Manula Alahakoon <manulaalahakoon@gmail.com>
2024-04-16 12:26:22 +0530

    Setup connection to MongoDB Atlas

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const PORT = 8000
 const express = require("express")
 const cors = require('cors')
+const { MongoClient } = require('mongodb')
+require('dotenv').config()
 const app = express()
 
 app.use(cors())
+app.use(express.json())
+
+const uri = process.env.URI
+
+const client = new  MongoClient(uri)
+
 
 app.listen(PORT,()=> console.log("App is listening to PORT",PORT))
