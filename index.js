9c9b02e20f973a7940cb71289a29730de4d8bb78
Manula Alahakoon <manulaalahakoon@gmail.com>
2024-04-17 17:41:10 +0530

    Backend POST to the database  as client sign up info

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const PORT = 8000
 const express = require("express")
 const cors = require('cors')
 const { MongoClient } = require('mongodb')
+const { v4: uuidv4 } = require('uuid')
+const bcrypt = require('bcrypt')
 require('dotenv').config()
 const app = express()
 
@@ -9,18 +11,29 @@ app.use(cors())
 app.use(express.json())
 
 const uri = process.env.URI
+//const client = new  MongoClient(uri)
 
-const client = new  MongoClient(uri)
+const connectToMongoDB = async (client) => {
+    try {
+        await client.connect();
+        console.log('Connected to MongoDB');
+        // Additional code for interacting with MongoDB can go here
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    }
+};
 
 //Sign up page
 app.post('/signup', async (req, res) => {
-    await client.connect()
+    //await client.connect()
     const { email, password } = req.body
-    const salt = bcript.genSaltSync(10)
-    const hash = bcrypt.hashSync(password, salt)
+    const salt = bcrypt.genSaltSync(10)
+    const hashedPassword = bcrypt.hashSync(password, salt)
+    const generatedUsereId = uuidv4()
     
     try {  
         const client = new MongoClient(uri)
+        await connectToMongoDB(client)
         const database = client.db('locofy_app_db')
         const users = database.collection("users")
         const exsistingUser = await users.findOne({ email })
@@ -28,7 +41,17 @@ app.post('/signup', async (req, res) => {
         if (exsistingUser) {
             return res.status(409).send("User already exist!")
         }
-        
+
+        const sanitizedEmail = email.toLowerCase()
+
+        const data = {
+            user_id: generatedUsereId,
+            email: sanitizedEmail,
+            hashed_password: hashedPassword       
+        }
+
+        const insertedUser = await users.insertOne(data)
+       
     } catch (error) {
         console.error(error)
     } finally {
@@ -36,4 +59,4 @@ app.post('/signup', async (req, res) => {
     }
 })
 
-app.listen(PORT,()=> console.log("App is listening to PORT",PORT))
+app.listen(PORT, () => console.log("App is listening to PORT", PORT))
